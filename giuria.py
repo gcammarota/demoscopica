e9f09398f108e0cf3ac43192f235f74f52698b11
Giuseppe Cammarota <g.cammarota@bopen.eu>
2023-12-21 15:57:13 +0100

    first auth test

--- giuria.py
+++ giuria.py
@@ -1,27 +1,46 @@
 # Copyright (c) 2023 Giuseppe Cammarota
 
-from requests_oauthlib import OAuth2Session
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 import streamlit as st
 
-
-REDIRECT_URI = "https://demoscopica.streamlit.app/"
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-
-
-def authorize():
-    google = OAuth2Session(st.secrets["CLIENT_ID"], redirect_uri=REDIRECT_URI, scope=SCOPES)
-    authorization_url, state = google.authorization_url(
-        "https://accounts.google.com/o/oauth2/v2/auth",
-        access_type="offline",
-        prompt="select_account"
-    )
-    st.markdown(f"[Login with Google]({authorization_url})")
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "openid", "https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"]
+CREDENTIAL_KEY = "credentials"
 
 
-def run():
-    st.write("# Giuria Demoscopica Privata 👋")
-    authorize()
+def get_client_config():
+    return {
+        "web": {
+            "client_id": st.secrets["CLIENT_ID"],
+            "client_secret": st.secrets["CLIENT_SECRET"],
+            "redirect_uris": ["https://demoscopica.streamlit.app/", "http://localhost:8501/", "http://localhost:8500/"],
+            "javascript_origins": ["http://demoscopica.streamlit.app:8501", "https://demoscopica.streamlit.app:8501", "http://localhost:8501"],
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://accounts.google.com/o/oauth2/token"
+        }
+    }
+
+def get_credentials():
+    if st.session_state.get(CREDENTIAL_KEY) is not None:
+        return
+    if "token" in st.session_state:
+        st.session_state[CREDENTIAL_KEY] = Credentials.from_authorized_user_info(st.session_state["token"], SCOPES)
+    credentials = st.session_state.get(CREDENTIAL_KEY)
+    # If there are no (valid) credentials available, let the user log in.
+    if credentials is None or not credentials.valid:
+        if credentials is not None and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_config(get_client_config(), SCOPES)
+            credentials = flow.run_local_server(port=8500)
+            st.session_state[CREDENTIAL_KEY] = credentials
+        # Save the credentials for the next run
+        st.session_state["token"] = credentials.to_json()
 
 
 if __name__ == "__main__":
-    run()
+    st.title("# Giuria Demoscopica Privata 👋")
+    get_credentials()
+    st.write(st.session_state)
+
