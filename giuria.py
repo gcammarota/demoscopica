1e4aeeed817c2a72dc3fcd222b8b4b8396bd243e
Giuseppe Cammarota <g.cammarota@bopen.eu>
2023-12-22 22:31:49 +0100

    fix login

--- giuria.py
+++ giuria.py
@@ -1,25 +1,27 @@
 # Copyright (c) 2023 Giuseppe Cammarota
 
+import sys
+
 from dotenv import dotenv_values
-from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import Flow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 import streamlit as st
 
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "openid", "https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"]
+
 CREDENTIAL_KEY = "credentials"
+REDIRECT_URIS = ["https://demoscopica.streamlit.app/", "http://localhost:8501/", "http://localhost:8500/"]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "openid", "https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"]
 
 
-def get_client_config():
-    client_config = {"web": {k: v for k, v in dotenv_values("client_secret.env").items()}}
-    client_config["web"].update({
-        "redirect_uris": ["https://demoscopica.streamlit.app/", "http://localhost:8501/", "http://localhost:8500/"],
-    })
+def read_client_config_env_file(client_config):
+    client_config = {"web": {k: v for k, v in dotenv_values(client_config).items()}}
+    client_config["web"].update({"redirect_uris": REDIRECT_URIS, "redirect_uri": [REDIRECT_URIS[1]]})
     return client_config
 
-def get_credentials():
-    if st.session_state.get(CREDENTIAL_KEY) is not None:
-        return
+
+def get_credentials(client_config):
     if "token" in st.session_state:
         st.session_state[CREDENTIAL_KEY] = Credentials.from_authorized_user_info(st.session_state["token"], SCOPES)
     credentials = st.session_state.get(CREDENTIAL_KEY)
@@ -28,15 +30,33 @@ def get_credentials():
         if credentials is not None and credentials.expired and credentials.refresh_token:
             credentials.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_config(get_client_config(), SCOPES)
-            credentials = flow.run_local_server(port=8500)
-            st.session_state[CREDENTIAL_KEY] = credentials
-        # Save the credentials for the next run
-        st.session_state["token"] = credentials.to_json()
+            flow = Flow.from_client_config(read_client_config_env_file(client_config), SCOPES)
+            flow.redirect_uri = REDIRECT_URIS[1]
+            url, _ = flow.authorization_url()
+            st.link_button("Accedi a Goole", url, type="primary")
+            user_info = None
+            params = st.experimental_get_query_params()
+            if params:
+                token = flow.fetch_token(code=params["code"][0])
+                credentials = Credentials(token["access_token"])
+                st.session_state[CREDENTIAL_KEY] = credentials
+                user_info_service = build('oauth2', 'v2', credentials=credentials)
+                user_info = user_info_service.userinfo().get().execute()
+                # Save the credentials for the next run
+                st.session_state["token"] = token
+            st.write(user_info)
 
+
+def run(client_config):
+    st.title("# Giuria Demoscopica Privata 👋")
+    if st.session_state.get(CREDENTIAL_KEY) is None:
+        get_credentials(client_config=client_config)
+    else:
+        st.write(st.session_state)
 
 if __name__ == "__main__":
-    st.title("# Giuria Demoscopica Privata 👋")
-    get_credentials()
-    st.write(st.session_state)
+    client_config = "client_secret.env"
+    if len(sys.argv) > 1:
+        client_config = sys.argv[1]
+    run(client_config=client_config)
 
