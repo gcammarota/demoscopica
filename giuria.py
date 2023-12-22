5cfe8a784fb594d140de5b7269da4d96a31b9261
Giuseppe Cammarota <g.cammarota@bopen.eu>
2023-12-22 11:42:30 +0100

    use external secrets

--- giuria.py
+++ giuria.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2023 Giuseppe Cammarota
 
+from dotenv import dotenv_values
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -10,16 +11,12 @@ CREDENTIAL_KEY = "credentials"
 
 
 def get_client_config():
-    return {
-        "web": {
-            "client_id": st.secrets["CLIENT_ID"],
-            "client_secret": st.secrets["CLIENT_SECRET"],
-            "redirect_uris": ["https://demoscopica.streamlit.app/", "http://localhost:8501/", "http://localhost:8500/"],
-            "javascript_origins": ["http://demoscopica.streamlit.app:8501", "https://demoscopica.streamlit.app:8501", "http://localhost:8501"],
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://accounts.google.com/o/oauth2/token"
-        }
-    }
+    client_config = dotenv_values("client_secret.env")
+    client_config.update({
+        "redirect_uris": ["https://demoscopica.streamlit.app/", "http://localhost:8501/", "http://localhost:8500/"],
+        "javascript_origins": ["http://demoscopica.streamlit.app:8501", "https://demoscopica.streamlit.app:8501", "http://localhost:8501"]
+    })
+    return client_config
 
 def get_credentials():
     if st.session_state.get(CREDENTIAL_KEY) is not None:
